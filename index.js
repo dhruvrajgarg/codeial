2e0007e20bb3d9b74fbb53cf829a8912f892b307
Dhruv Raj Garg <dhruv.rajgarg@ymail.com>
2020-06-14 01:22:09 +0530

    Added Static files support

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
 const port = 8000;
 
-const expressLayouts = require('express-ejs-layouts');
+//Setting Up Ststics
+app.use(express.static("./assets"));
+
+//Use Express Layouts
+const expressLayouts = require("express-ejs-layouts");
 app.use(expressLayouts);
 
+//Ectract Styles and Scripts from sub-pages into layouts
+app.use("layout extractStyles", true);
+app.use("layout extractScripts", true);
+
 // Use Express Router
-app.use('/' , require('./routes'));
+app.use("/", require("./routes"));
 
 //Set View Engine
-app.set('view engine', 'ejs');
-app.set('views' , './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-
-app.listen(port, function(err){
-    if(err){
-        console.log(`Error in running the Server: ${err}`);
-        return;
-    }
-    console.log("Server Running Successfully on port : " , port);    
-})
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the Server: ${err}`);
+    return;
+  }
+  console.log("Server Running Successfully on port : ", port);
+});
