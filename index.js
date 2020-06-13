649b7c2d2e0cd930604eb10a9032e19a4514d542
dhruvrajgarg <dhruv.rajgarg@ymail.com>
2020-06-13 10:06:44 +0530

    Added posts controller and route

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express();
 
 const port = 8000;
 
-// Use Express.Router
+// Use Express Router
 app.use('/' , require('./routes'));
+
+//Set View Engine
+app.set('view engine', 'ejs');
+app.set('views' , './views');
 
 
 app.listen(port, function(err){
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require('../controllers/users_controller')
-console.log("Router Loaded");
+
 
 router.get('/profile' , usersController.profile);
 
