7ec2b92d91518372ad3bc9ac0c99c3e3308b1b3f
Dhruv Raj Garg <dhruv.rajgarg@ymail.com>
2020-06-24 03:02:03 +0530

    Added Complete Comments Support

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,20 +1,26 @@
 const Post = require("../models/post");
 
 module.exports.home = function (req, res) {
-  // console.log(req.cookies);
-  // res.cookie("user_id", 25);
-  Post.find({}).populate('user').exec(function(err , fetchedPosts){
-    if(err){
-      console.log("Error in Fetching posts" , err);
-      return;
-    }
-    //console.log(Post.populated('user'));
-    
-    return res.render("home", {
-      title: "Home",
-      posts: fetchedPosts,
+  Post.find({})
+    .populate("user")
+    .populate({
+      path : "comments",
+      populate : {
+        path : "user"
+      }
+    })
+    .exec(function (err, fetchedPosts) {
+      if (err) {
+        console.log("Error in Fetching posts", err);
+        return;
+      }
+      //console.log(Post.populated('user'));
+
+      return res.render("home", {
+        title: "Home",
+        posts: fetchedPosts,
+      });
     });
-  })
 };
 
 // module.exports.actionName = function(req, res){}
