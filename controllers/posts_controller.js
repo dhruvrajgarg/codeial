efc5f47ac6789aa64751e65f4a5be73557b2ad7d
Dhruv Raj Garg <dhruv.rajgarg@ymail.com>
2020-06-26 01:55:15 +0530

    code broken into partials

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,9 +1,9 @@
 const Post = require("../models/post");
-
+const Comment = require("../models/comment");
 
 module.exports.create = function (req, res) {
   console.log(req.user);
-  
+
   Post.create(
     {
       content: req.body.content,
@@ -17,4 +17,21 @@ module.exports.create = function (req, res) {
       res.redirect("back");
     }
   );
+};
+
+module.exports.destroy = function (req, res) {
+  Post.findById(req.params.id, function (err, post) {
+    if (err) {
+      console.log("Error in deleting post");
+      return;
+    }
+    if (post.user == req.user.id) {
+      post.remove();
+      Comment.deleteMany({ post: req.params.id }, function (err) {
+        return res.redirect("back");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
 };
